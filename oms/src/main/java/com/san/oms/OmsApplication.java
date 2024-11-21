3ad149790ddf8e5366f38c5e03c0178f18628cc4
Sang Hoang Nguyen <sang.h.nguyen@endava.com>
2024-11-21 15:10:49 +0700

    Init repository

---
+++ oms/src/main/java/com/san/oms/OmsApplication.java
@@ -0,0 +1,13 @@
+package com.san.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsApplication.class, args);
+	}
+
+}
