daea7d98a149c4f4a56b1bd2462e8c086b838c2c
Sang Hoang Nguyen <sang.h.nguyen@endava.com>
2024-11-28 16:13:28 +0700

    Implement some basic APIs for the Product, trying to apply SOLID, if any

---
+++ oms/customer/src/main/java/com/personal/Customer/controllers/CustomerController.java
@@ -0,0 +1,13 @@
+package com.personal.Customer.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerController.class, args);
+    }
+
+}
